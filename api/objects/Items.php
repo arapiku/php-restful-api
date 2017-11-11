fde482fc03ec2eb487c9b4c8ecae887c7a53f9f6
Arata Kenta <aratakenta@team-lab.com>
2017-11-11 13:02:19 +0000

    createメソッドを追加

--- api/objects/Items.php
+++ api/objects/Items.php
@@ -16,8 +16,9 @@ class Items {
     $this->conn = $db;
   }
 
+  // 全件取得メソッド
   function read() {
-    // 全てのクエリを選択
+    // 全てのレコードを選択するクエリ
     $query = "SELECT * FROM " . $this->table_name;
 
     // クエリのステートメントを用意
@@ -27,5 +28,31 @@ class Items {
     $stmt->execute();
 
     return $stmt;
+  }
+
+  // 新規作成メソッド
+  function create() {
+    // レコードを挿入するクエリ
+    // $query = "INSERT INTO " . $this->table_name . "SET title=:title, description=:description, price=:price, image=:image";
+    $query = "INSERT INTO " . $this->table_name . "(title, description, price, image) VALUES (:title, :description, :price, :image)";
+
+    // クエリのステートメントを用意
+    $stmt = $this->conn->prepare($query);
+
+    // POSTされた商品データを取得
+    $data = json_decode(file_get_contents("php://input"), true);
+
+    // パラメータをバインド
+    $stmt->bindParam(":title", $data['title']);
+    $stmt->bindParam(":description", $data['description']);
+    $stmt->bindParam(":price", $data['price']);
+    $stmt->bindParam(":image", $data['image']);
+
+    // クエリを実行
+    if($stmt->execute()) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
