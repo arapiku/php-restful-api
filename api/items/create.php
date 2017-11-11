f3c7053171680ff07248451e7dcbb9b809a7aec1
Arata Kenta <aratakenta@team-lab.com>
2017-11-11 13:01:57 +0000

    新規作成をするcreate.phpを作成

---
+++ api/items/create.php
@@ -0,0 +1,31 @@
+<?php
+// headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Method: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+// データベースファイルとオブジェクトファイルを読み込む
+include_once '../config/database.php';
+include_once '../objects/Items.php';
+
+// dbのインスタンスを作成
+$database = new Database();
+$db = $database->getConnection();
+
+// オブジェクトを初期化
+$items = new Items($db);
+
+// 投稿された商品データを作成
+if($items->create()) {
+  http_response_code(201);
+  echo '{';
+    echo '"message": "Items was created."';
+  echo '}';
+} else {
+  http_response_code(409);
+  echo '{';
+    echo '"message": "Unable to create items."';
+  echo '}';
+}
