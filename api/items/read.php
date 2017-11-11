cb1cd500a07e71f587b6f59a4374681ecaa9db12
Arata Kenta <aratakenta@team-lab.com>
2017-11-11 06:39:37 +0000

    オブジェクトを初期化以降の記述が反映されてなかったのでリトライ

--- api/items/read.php
+++ api/items/read.php
@@ -13,3 +13,39 @@ $db = $database->getConnection();
 
 // オブジェクトを初期化
 $items = new Items($db);
+
+// クエリ
+$stmt = $items->read();
+$num = $stmt->rowCount();
+
+// もしレコードが1件以上なら
+if($num > 0) {
+
+  // 配列を生成
+  $items_arr = [];
+  $items_arr["records"] = [];
+
+  // db検索
+  while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    // 配列からシンボルテーブルに変数をインポート
+    extract($row);
+
+    // 商品データ
+    $items_data = [
+      "id" => $id,
+      "title" => $title,
+      "description" => html_entity_decode($description),
+      "price" => $price,
+      "image" => $image
+    ];
+
+    // 配列に格納
+    array_push($items_arr["records"], $items_data);
+  }
+
+  echo json_encode($items_arr);
+} else {
+  echo json_encode(
+      ["message" => "No items found."]
+  );
+}
