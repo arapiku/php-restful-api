89c7c83845ca8890144b94efe803143b78fed506
Arata Kenta <aratakenta@team-lab.com>
2017-11-12 03:32:40 +0000

    商品を文字列検索するsearch.phpを作成

---
+++ api/items/search.php
@@ -0,0 +1,54 @@
+<?php
+// headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+// データベースファイルとオブジェクトファイルを読み込む
+include_once '../config/database.php';
+include_once '../objects/Items.php';
+
+// dbのインスタンスを作成
+$database = new Database();
+$db = $database->getConnection();
+
+// オブジェクトを初期化
+$items = new Items($db);
+
+// キーワードを取得
+$keywords = isset($_GET["s"]) ? $_GET["s"] : "";
+
+// クエリを呼び出す
+$stmt = $items->search($keywords);
+$num = $stmt->rowCount();
+
+// もしレコードが1件以上あれば
+if($num > 0) {
+  
+  // 配列を生成
+  $items_arr = [];
+  $items_arr["records"] = [];
+
+  // DB検索
+  while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    // 配列からシンボルテーブルに変数をインポート
+    extract($row);
+
+    // 商品データ
+    $items_data = [
+      "id" => $id,
+      "title" => $title,
+      "description" => html_entity_decode($description),
+      "price" => $price,
+      "image" => $image
+    ];
+
+    // 配列に格納
+    array_push($items_arr["records"], $items_data);
+  }
+
+  echo json_encode($items_arr);
+// レコードが1件もなければ
+} else {
+  http_response_code(404);
+  echo json_encode(["message" => "No items found"]);
+}
